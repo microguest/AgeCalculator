94e4af01954051b92313ce5257fa9808b658984a
武亚军 <microguest@126.com>
2015-10-02 14:12:10 +0800

    add robojuice

---
+++ AgeCalculator/app/src/main/java/com/microguest/activitys/ACBaseActivity.java
@@ -0,0 +1,46 @@
+package com.microguest.activitys;
+
+import android.os.Bundle;
+
+import roboguice.activity.RoboActivity;
+
+/**
+ * Created by wuyajun on 15/10/2.
+ */
+public class ACBaseActivity extends RoboActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
--- AgeCalculator/app/src/main/java/com/microguest/activitys/MainActivity.java
+++ AgeCalculator/app/src/main/java/com/microguest/activitys/MainActivity.java
@@ -1,19 +1,27 @@
 package com.microguest.activitys;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.microguest.R;
+import com.microguest.utils.PropertiesConfig;
+
+import roboguice.inject.InjectView;
 
 /**
  * Created by wuyajun on 15/10/2.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends ACBaseActivity {
+
+    @InjectView(R.id.text)
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text.setText("" + PropertiesConfig.getInstance(this).isDeveloper());
     }
 
 
